b7229cc2b6daaec97a75429ffa95805e04b30e47
Pranprest <79233261+Pranprest@users.noreply.github.com>
2022-06-01 23:02:51 -0300

    Remove redundant file operations and variables

--- src/booky/booky.py
+++ src/booky/booky.py
@@ -7,21 +7,22 @@
     ===
 """
 from argparse import Namespace, ArgumentParser
+from mmap import mmap, ACCESS_READ
+from shutil import copyfileobj
 from typing import Final
 from pathlib import Path
 import json
 import sys
-import os
 
 
-def argParser() -> ArgumentParser:
+def _arg_parser() -> ArgumentParser:
     _parser = ArgumentParser(
         description='Bookmark files and use them in other programs.', prog="Booky")
 
     # Manage bookmarks themselves
     _parser.add_argument('file',
                          type=str,
-                         nARGS="?",
+                         nargs="?",
                          metavar="File/Alias",
                          help="The file/alias that will be used")
 
@@ -37,11 +38,11 @@ def argParser() -> ArgumentParser:
                                     '--alias',
                                     type=str,
                                     metavar="ALIAS",
-                                    help="Bookmarks new file or updates existing one")
+                                    help="Alias that the file will be assigned to.")
 
     mutually_exclusive.add_argument('-r', '--remove',
                                     action="store_true",
-                                    help="Remove a bookmarked file under current environment")
+                                    help="Remove a bookmarked file under current ARGS.environment")
 
     # Do things with the bookmarked files
     mutually_exclusive.add_argument('-o', '--output',
@@ -54,117 +55,102 @@ def argParser() -> ArgumentParser:
 
     mutually_exclusive.add_argument('-l',
                                     '--list',
-                                    '--list-env',
+                                    '--list-ARGS.env',
                                     action="store_true",
-                                    help="Lists every alias in current environment")
+                                    help="Lists every alias in current ARGS.environment")
     return _parser
 
 
-def echoFile(FilePath: Path) -> None:
-    from mmap import mmap, ACCESS_READ
-    from shutil import copyfileobj
-    with open(FilePath, 'rb') as f:
+def echo_file(filepath: Path) -> None:
+    with open(filepath, 'rb') as f:
         with mmap(f.fileno(), 0, access=ACCESS_READ) as s:
             copyfileobj(s, sys.stdout.buffer)
 
 
 def main() -> None:
-    # Create json data file if does'nt exist (or is empty) and reference it in data file
-    main_data_file: Final[Path] = Path(f"{Path(__file__).parent.absolute()}/data.json")
-    if not main_data_file.exists():
-        with open(main_data_file, 'w') as f:
-            f.write("{}")
-
     # Parse the args!
-    ARGS: Final[Namespace] = argParser().parse_args()
-    env: str = ARGS.env
+    ARGS: Final[Namespace] = _arg_parser().parse_args()
 
-    # Check if env is actually in there, if not, create new one.
-    if ARGS.env is not None:
+    # Create json data file if does'nt exist (or is empty) and reference it in data file
+    main_data_file: Final[Path] = Path(f"{Path(__file__).parent.resolve()}/data.json")
+    if not main_data_file.exists():
+        try:
+            with open(main_data_file, 'w') as f:
+                f.write("{}")
+        except OSError:
+            sys.exit(f"Error trying to write default data to {main_data_file}.")
+
+    try:
         with open(main_data_file, "r+") as f:
-            full_data: dict = json.load(f)
-            if env not in full_data:
-                full_data[env] = {}
-                f.seek(0)
-                json.dump(full_data, f, indent=4)
-                f.truncate()
+            app_data: dict = json.load(f)
+    except OSError:
+        sys.exit(f"Error trying to read {main_data_file}.")
 
-    # If ARGS.file is an alias, set it to the alias'es filepath
-    # And make a variable called filepath
-    original_alias = ARGS.file
+    if ARGS.env is not None and ARGS.env not in app_data:
+        # Then create a new one!
+        app_data[ARGS.env] = {}
+        f.seek(0)
+        json.dump(app_data, f, indent=4)
+        f.truncate()
+
     if ARGS.file is not None:
-        # Share this single "read" call for other every argument
-        with open(main_data_file, "r") as f:
-            full_data = json.load(f)
-        if ARGS.file in full_data[env]:
-            ARGS.file = Path(full_data[env][ARGS.file])
+        # If ARGS.file is an alias, set it to the alias'es file
+        if ARGS.file in app_data[ARGS.env]:
+            ARGS.file = Path(app_data[ARGS.env][ARGS.file]).resolve()
         else:
-            ARGS.file = Path(ARGS.file)
-        # If it somehow isn't an absolute path, make it absolute anyways
-        filepath = ARGS.file.absolute()
+            ARGS.file = Path(ARGS.file).resolve()
 
-    # Mutulally exclusive arguments:
-    # Print alias'es folder, if not, print all of env's variables
     if ARGS.list:
-        if ARGS.file is not None:
-            if filepath.is_dir():
-                print(*filepath.iterdir())
-                sys.exit()
-        print(f"These are the aliases within env '{env}':")
-        for key, val in full_data[env].items():
+        # Print alias' folder, if not, print all of current env's variables
+        if ARGS.file is not None and ARGS.file.is_dir():
+            for path in list(ARGS.file.iterdir()):
+                print(path.resolve())
+            sys.exit(0)
+        print(f"These are the aliases within environment '{ARGS.env}':")
+        for key, val in app_data[ARGS.env].items():
             print(f'"{key}": "{val}"')
         sys.exit(0)
 
-    # Depending on which kind of file this is, output something to stdout
     if ARGS.output and ARGS.file is not None:
-        if not filepath.exists():
-            print("Not a valid file or directory")
-            sys.exit(1)
-        if filepath.is_file():
-            echoFile(filepath)
+        if not ARGS.file.exists():
+            sys.exit("Not a valid file or directory")
+
+        if ARGS.file.is_file():
+            echo_file(ARGS.file)
         else:
-            print(*os.listdir(filepath))
+            for path in list(ARGS.file.iterdir()):
+                print(path.resolve())
         sys.exit(0)
 
-    # Adds keyword + file as an json key if it doesnt exist,
-    # otherwise, update existing one
-    if ARGS.add is not None and ARGS.file is not None:
-        if not filepath.exists():
-            sys.exit("Not a valid filepath, exiting.")
+    if ARGS.alias is not None and ARGS.file is not None:
+        if not ARGS.file.exists():
+            sys.exit(f"Not {ARGS.file} is not a valid file, exiting.")
 
-        alias = ARGS.add
-        full_data[env][alias] = str(filepath)
+        # (not necessary), just for readability: alias = ARGS.alias
+        app_data[ARGS.env][ARGS.alias] = str(ARGS.file)
         with open(main_data_file, "w") as f:
             f.seek(0)
-            json.dump(full_data, f, indent=4)
+            json.dump(app_data, f, indent=4)
             f.truncate()
 
-        # If alias already exists in env
-        update_alias = True if alias in full_data[env] else False
-        if update_alias:
-            print(f"Alias '{alias}' in env '{env}' updated successfully!")
+        if ARGS.alias in app_data[ARGS.env]:
+            print(f"Alias '{ARGS.alias}' in environment '{ARGS.env}' updated successfully!")
         else:
-            print(
-                f"Alias '{alias}' added to environment '{env}' successfully!")
+            print(f"Alias '{ARGS.alias}' added to environment '{ARGS.env}' successfully!")
         sys.exit(0)
 
-    # Removes alias from data file
     if ARGS.remove and ARGS.file is not None:
-        with open(main_data_file, "r+") as f:
-            if original_alias not in full_data[env]:
-                sys.exit(f'Alias {original_alias} not found under env "{env}"')
-            else:
-                del full_data[env][original_alias]
-                f.seek(0)
-                json.dump(full_data, f, indent=4)
-                f.truncate()
-                print(f'Alias {original_alias} sucessfully removed from env {env}!')
-                sys.exit(0)
+        if ARGS.file not in app_data[ARGS.env]:
+            sys.exit(f'Alias {ARGS.file} not found under environment {ARGS.env}')
+        del app_data[ARGS.env][ARGS.file]
+        f.seek(0)
+        json.dump(app_data, f, indent=4)
+        f.truncate()
+        print(f'Alias {ARGS.file} sucessfully removed from environment {ARGS.env}!')
+        sys.exit(0)
 
-    # if there's no file and there was no other option, print help
-    # also, if nothing else than the file is specified, run --path
     if ARGS.file is not None:
-        print(filepath)
+        print(ARGS.file)
         sys.exit(0)
 
 
